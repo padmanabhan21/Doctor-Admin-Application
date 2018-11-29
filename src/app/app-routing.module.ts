28756da240318920f7b96e7996b840b527ee168c
infocuittesting <infocuit.testing@gmail.com>
2018-11-29 11:59:16 +0530

    removed unnessary routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,41 +10,9 @@ const routes: Routes = [
         path: 'configer',
         loadChildren: './configuration/configuration.module#ConfigurationModule'
     },
-     // {
-    //     path: 'ecommerce',
-    //     loadChildren: 'app/ecommerce/ecommerce.module#EcommerceModule'
-    // },
-    {
-        path: 'user-interface',
-        loadChildren: 'app/user-interface/user-interface.module#UserInterfaceModule'
-    },
-    {
-        path: 'forms',
-        loadChildren: 'app/forms/forms.module#FormsModule'
-    },
-    {
-        path: 'tables',
-        loadChildren: 'app/tables/tables.module#TablesModule'
-    },
-    {
-        path: 'charts',
-        loadChildren: 'app/charts/charts.module#ChartsModule'
-    },
-    {
-        path: 'widgets',
-        loadChildren: 'app/widgets/widgets.module#WidgetsModule'
-    },
     {
         path: 'authentication',
         loadChildren: './authentication/authentication.module#AuthenticationModule'
-    },
-    {
-        path: 'sample-pages',
-        loadChildren: 'app/sample-pages/sample-pages.module#SamplePagesModule'
-    },
-    {
-        path: 'maps',
-        loadChildren: 'app/maps/maps.module#MapsModule'
     },
     {
         path: '',

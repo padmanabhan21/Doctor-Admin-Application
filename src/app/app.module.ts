c7412381496c2a79753485e93e002a8261717aae
infocuittesting <infocuit.testing@gmail.com>
2018-11-29 12:55:48 +0530

    new patient-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PatientlistComponent } from './patient list/patientlist/patientlist.component';
 // import { ConfigurationComponent } from './configuration/configuration/configuration.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PatientlistComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/patient list/patientlist.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatientlistRoutingModule } from './patientlist-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PatientlistRoutingModule
+  ]
+})
+export class PatientlistModule { }

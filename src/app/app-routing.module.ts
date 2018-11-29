14f076affa22b20fe065304353f02dd8e0f41b35
infocuittesting <infocuit.testing@gmail.com>
2018-11-29 11:20:45 +0530

    new cheanges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,38 +14,35 @@ const routes: Routes = [
     //     path: 'ecommerce',
     //     loadChildren: 'app/ecommerce/ecommerce.module#EcommerceModule'
     // },
-    {
-        path: 'user-interface',
-        loadChildren: 'app/user-interface/user-interface.module#UserInterfaceModule'
-    },
-    {
-        path: 'forms',
-        loadChildren: 'app/forms/forms.module#FormsModule'
-    },
-    {
-        path: 'tables',
-        loadChildren: 'app/tables/tables.module#TablesModule'
-    },
-    {
-        path: 'charts',
-        loadChildren: 'app/charts/charts.module#ChartsModule'
-    },
-    {
-        path: 'widgets',
-        loadChildren: 'app/widgets/widgets.module#WidgetsModule'
-    },
+    // {
+    //     path: 'user-interface',
+    //     loadChildren: 'app/user-interface/user-interface.module#UserInterfaceModule'
+    // },
+    // {
+    //     path: 'forms',
+    //     loadChildren: 'app/forms/forms.module#FormsModule'
+    // },
+    // {
+    //     path: 'tables',
+    //     loadChildren: 'app/tables/tables.module#TablesModule'
+    // },
+    // {
+    //     path: 'charts',
+    //     loadChildren: 'app/charts/charts.module#ChartsModule'
+    // },
+    // {
+    //     path: 'widgets',
+    //     loadChildren: 'app/widgets/widgets.module#WidgetsModule'
+    // },
     {
         path: 'authentication',
         loadChildren: './authentication/authentication.module#AuthenticationModule'
     },
-    {
-        path: 'sample-pages',
-        loadChildren: 'app/sample-pages/sample-pages.module#SamplePagesModule'
-    },
-    {
-        path: 'maps',
-        loadChildren: 'app/maps/maps.module#MapsModule'
-    },
+    
+    // {
+    //     path: 'maps',
+    //     loadChildren: 'app/maps/maps.module#MapsModule'
+    // },
     {
         path: '',
         redirectTo: 'dashboard',

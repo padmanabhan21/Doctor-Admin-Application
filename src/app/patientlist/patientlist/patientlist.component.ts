66ad374bb060ac6e05e99aaae6d1aab3c866ee88
padmanabhan21 <padmanabhan210193@gmail.com>
2018-11-29 16:12:08 +0530

    new patientlist

--- src/app/patientlist/patientlist/patientlist.component.ts
+++ src/app/patientlist/patientlist/patientlist.component.ts
@@ -11,5 +11,10 @@ export class PatientlistComponent implements OnInit {
 
   ngOnInit() {
   }
-
+//Patient List
+public patientListData = [
+  {"customer_name":"Bala","customer_token":1,"customer_mobile":"822-077-2736","customer_address":"21A/2 Kanchipuram,TamilNadu-631502"},
+  {"customer_name":"Venkat","customer_token":2,"customer_mobile":"832-804-6698","customer_address":"123 Nellore,Andra Pradesh-44"},
+  {"customer_name":"Santha","customer_token":3,"customer_mobile":"822-077-2736","customer_address":"25/11 New Colony,Chromepet,TVM-60601"}
+  ]
 }
